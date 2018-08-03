9b6c1d0bb9f5ad0917edfb45d22c162e1b979f96
Tri Wijayanto <triwijayanto90@gmail.com>
2018-08-03 09:26:09 +0700

    [Exercises 4] Mengenal penggunaan Conditional (Switch-Case) dalam JavaScript

---
+++ [Exercises4].js
@@ -0,0 +1,46 @@
+// gantilan nilai variable bulan
+var hari = 31
+var bulan = 6
+var tahun = 1993
+
+// switch case hanya untuk variable bulan
+switch (bulan) {
+  case 1:
+    console.log(hari, 'Januari', tahun);
+    break;
+  case 2:
+    console.log(hari, 'Februari', tahun);
+    break;
+  case 3:
+    console.log(hari, 'Maret' , tahun);
+    break;
+  case 4:
+    console.log(hari, 'April' , tahun);
+    break;    
+  case 5:
+    console.log(hari, 'Mei' , tahun);
+    break;    
+  case 6:
+    console.log(hari, 'Juni' , tahun);
+    break;    
+  case 7:
+    console.log(hari, 'Juli' , tahun);
+    break;                       
+  case 8:
+    console.log(hari, 'Agustus' , tahun);
+    break;    
+  case 9:
+    console.log(hari, 'September' , tahun);
+    break;    
+  case 10:
+    console.log(hari, 'Oktober' , tahun);
+    break;    
+  case 11:
+    console.log(hari, 'November' , tahun);
+    break;
+  case 12:
+    console.log(hari, 'Desember' , tahun);
+    break;                            
+  default:
+    console.log('Kamu tidak memilih bulan secara benar');
+}
