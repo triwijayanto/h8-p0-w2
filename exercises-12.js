64c5d9a43479bf20fd1930b8d4804fd923895bdf
Tri Wijayanto <triwijayanto90@gmail.com>
2018-08-05 21:13:41 +0700

    [Exercises 12] Konversi Menit

--- exercises-12.js
+++ exercises-12.js
@@ -1,6 +1,10 @@
 function konversiMenit(menit) {
-  
+
+  if (menit%60<10){
+    return parseInt(menit/60) +':'+'0'+(menit%60)
+  } else {
     return parseInt(menit/60) +':'+(menit%60)
+  }
   
   }
   
@@ -10,3 +14,5 @@ function konversiMenit(menit) {
   console.log(konversiMenit(53)); // 0:53
   console.log(konversiMenit(88)); // 1:28
   console.log(konversiMenit(120)); // 2:00
+
+  
