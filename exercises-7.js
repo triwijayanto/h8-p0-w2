04d3efcbfb570bb4fe52dc34dbcf791a5c37fa72
Tri Wijayanto <triwijayanto90@gmail.com>
2018-08-05 18:22:28 +0700

    [Exercises 7] Perulangan - Play with Asterisks

--- exercises-7.js
+++ exercises-7.js
@@ -8,5 +8,32 @@ for(var angka = 1; angka <= rows; angka++) {
 
 console.log() // spasi
 
+
 // 2. Menyusun Barisan Bintang Dengan Nested Looping
+
+var stringKosong = ''
+
+//untuk membuat bintang sebanyak rows
+for (var nomorDua = 1; nomorDua <= rows; nomorDua++) {
+   stringKosong += '*'
+}
+
+//untuk mencetak bintang sebanyak rows
+for (var ulang = 0; ulang < rows; ulang++) {
+ console.log(stringKosong)
+}
+
+console.log() // spasi
 // 3. Menyusun Barisan Tangga Bintang Dengan Nested Looping
+
+
+for (var nomorTiga = 1; nomorTiga <= rows; nomorTiga++) {
+    var stringKosong = '';
+
+
+for (var segitiga = 1; segitiga <= nomorTiga; segitiga++) {
+      stringKosong += '*';
+    }
+
+    console.log(stringKosong);
+}
