e8fbac1e61233f6dae36efee1fa23874337f7c90
Tri Wijayanto <triwijayanto90@gmail.com>
2018-08-05 12:11:18 +0700

    [Exercises 3] Mengenal penggunaan Conditional if else dalam JavaScript

--- exercises-3.js
+++ exercises-3.js
@@ -1,19 +1,19 @@
-var peran = 'Penyihir'
-var nama = 'Joni'
+var peran = 'Ksatria'
+var nama = 'Tri'
 
 if (peran === '' && nama === ''){
     console.log('Nama harus diisi')
-} else if (peran === '' && nama === 'Joni') {
+} else if (peran === '' && nama) {
     console.log('Halo', nama,',','Pilih peranmu untuk memulai game!')
-} else if(peran==='Ksatria' && nama === 'Joni') {
+} else if(peran ==='Ksatria' && nama) {
     console.log('Selamat datang di Dunia Proxitia,', nama,'!')
     console.log('Halo Ksatria', nama+',', 'kamu dapat menyerang dengan senjatamu!')
-} else if(peran==='Tabib' && nama === 'Joni'){
+} else if(peran==='Tabib' && nama){
     console.log('Selamat datang di Dunia Proxitia,', nama,'!')
     console.log('Halo Tabib', nama+',', 'Kamu akan membantu temanmu yang terluka.');
-} else if(peran==='Penyihir' && nama === 'Joni'){
+} else if(peran==='Penyihir' && nama){
     console.log('Selamat datang di Dunia Proxitia,', nama,'!')
     console.log('Halo Penyihir', nama+',', 'Ciptakan keajaiban yang membantu kemenanganmu!');
 } else {
-    console.log('Hayooo! ada yg salah pasti')
+    console.log('')
 }
