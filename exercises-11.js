54f1dd9181c1897b1c2cc6d7f23ae61291bb6da6
Tri Wijayanto <triwijayanto90@gmail.com>
2018-08-07 12:48:23 +0700

    [Exercises 11] Balik Kata

--- exercises-11.js
+++ exercises-11.js
@@ -1,10 +1,14 @@
 function balikKata(kata) {
-    return kata.split('').reverse().join("");
-}
-   
-   // TEST CASES
-console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
-console.log(balikKata('John Doe')); // eoD nhoJ
-console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
-console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
-console.log(balikKata('Super')); // repuS
+    var kosong =""
+    for(var i = kata.length - 1; i >=0 ; i --){
+      kosong += kata[i]
+    }
+    return kosong
+  }
+     
+     // TEST CASES
+  console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+  console.log(balikKata('John Doe')); // eoD nhoJ
+  console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+  console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+  console.log(balikKata('Super')); // repuS
